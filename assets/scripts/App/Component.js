4417d02d1ab2efe2f8cd9aa14b9fe6355d106ebe
Victor Shevchuk <v.shevchuk@viseven.com>
2021-06-09 07:30:18 +0400

    started working with modules

---
+++ assets/scripts/App/Component.js
@@ -0,0 +1,25 @@
+export class Component {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElement = document.getElementById(hostElementId);
+    } else {
+      this.hostElement = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
+
+  detach() {
+    if (!this.element) {
+      return;
+    }
+    this.element.remove();
+    // this.element.parentElement.removeChild(this.element); // for older browsers
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+}
